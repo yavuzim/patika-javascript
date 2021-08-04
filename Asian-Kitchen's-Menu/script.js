91c0083f2bcc2840ed6d2cb99968a6605409fd83
kemal <yavuz.imer133@gmail.com>
2021-08-04 21:18:35 +0300

    Asian-Kitchen's-Menu

---
+++ Asian-Kitchen's-Menu/script.js
@@ -0,0 +1,71 @@
+let foods = {
+  korea: [
+    {
+      id: 1,
+      title: "Tteokbokki",
+      category: "Korea",
+      price: 10.99,
+      img: "images/Ktteokbokki-500x500.jpg",
+    },
+    {
+      id: 2,
+      title: "Bibimbap",
+      category: "Korea",
+      price: 8.99,
+      img: "images/Kbibimbap.jpg",
+    },
+    {
+      id: 3,
+      title: "Jajangmyeon",
+      category: "Korea",
+      price: 15.99,
+      img: "images/KJajangmyeon.jpg",
+    },
+  ],
+  japan: [
+    {
+      id: 4,
+      title: "Chicken Ramen",
+      category: "Japan",
+      price: 7.99,
+      img: "images/Jchicken-ramen.jpg",
+    },
+    {
+      id: 5,
+      title: "Onigiri",
+      category: "Japan",
+      price: 9.99,
+      img: "images/Jchicken-ramen.jpg",
+    },
+    {
+      id: 6,
+      title: "Doroyaki",
+      category: "Japan",
+      price: 3.99,
+      img: "images/JDorayaki.jpg",
+    },
+  ],
+  china: [
+    {
+      id: 7,
+      title: "Dan Dan Mian",
+      category: "China",
+      price: 5.99,
+      img: "images/CDan-Dan-Mian.jpg",
+    },
+    {
+      id: 8,
+      title: "Yangzhou Fried Rice",
+      category: "China",
+      price: 12.99,
+      img: "images/CYangzhou-Fried-Rice.jpg",
+    },
+    {
+      id: 9,
+      title: "Ma Yi Shang Shu",
+      category: "China",
+      price: 12.99,
+      img: "images/CMa-Yi-Shang-Shu.jpg",
+    },
+  ],
+};
